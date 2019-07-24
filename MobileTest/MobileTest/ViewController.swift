02e168a6b6a5cf9ce2a8a6ca5a224f6de97bde17
Paulo Ricardo <praraujo.vieira@gmail.com>
2019-07-24 14:15:32 -0300

    project

---
+++ MobileTest/MobileTest/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MobileTest
+//
+//  Created by Paulo Ricardo on 24/07/19.
+//  Copyright © 2019 Paulo Ricardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    struct Cliente {
+        <#fields#>
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
